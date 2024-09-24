934eba87202cf50547550b3387ee2967d5deacab
Jacob Ford <90845996+jfordnull@users.noreply.github.com>
2024-09-24 17:21:24 -0500

    Update twoSum solution (forgot to save .cpp)

--- 1-TwoSum/twosum.cpp
+++ 1-TwoSum/twosum.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int> nums, int target){
-        unordered_map<int, int> delta;
+        map<int, int> delta;
         for (int i=0;i<nums.size();i++){
             if(delta.find(nums[i])!=delta.end()) return {delta.at(nums[i]),i};
-            delta.insert(pair<int,int>(target-nums[i],i));
+            delta[target-nums[i]]=i;
         }
         return {0,0};
     }
-    
 };
